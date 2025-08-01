748ee72b5dc989cc4b7f5bbd35cbdfb71c827f7f
Omar Collado <omarcollado154@gmail.com>
2025-07-31 19:06:12 -0500

    Made it where user and hex pfps now show up in chat like the in game counterparts and changed some styling.

--- static/modules/seperateadded.js
+++ static/modules/seperateadded.js
@@ -2,23 +2,25 @@
 //of structure for my addons >w>
 var audioPlayer = document.getElementById('audioPlayer');
 var hoverSounds = document.querySelectorAll(".hovers");
-
+var optionButtons = document.querySelectorAll("#options > button");
 var buttons = document.querySelectorAll(".amog");
 console.log(buttons[0]);
+console.log(optionButtons[0]);
 
 
+optionButtons.forEach(addAudioListener);
 buttons.forEach(addAudioListener);
 function addAudioListener(item, index) {
-    console.log(item);
-            buttons[index].addEventListener('mouseenter', hoverSoundSelection);
+            item.addEventListener('mouseenter', hoverSoundSelection);
 }
 
 function hoverSoundSelection(){
     var randNum = (Math.floor(Math.random() * 5));
-    hoverSounds[randNum].load();
-    hoverSounds[randNum].play().catch(error => {
+    if(this.disabled != true) {
+      hoverSounds[randNum].load();
+      hoverSounds[randNum].play().catch(error => {
       console.error('Playback failed: amogus', error);
-    });
+    });}
 }
 function play(){
     audioPlayer.load(); // reload the audio player so that the audio restarts from the beginning on each click
